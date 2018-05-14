d10f2e09113fac5566408989b5aabbac421e90f9
Kyle Evans <evans.584@osu.edu>
2018-05-13 21:20:21 -0400

    initial commit

---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'kbe_login_banner::default' do
+  # Serverspec examples can be found at
+  # http://serverspec.org/resource_types.html
+  it 'does something' do
+    skip 'Replace this with meaningful tests'
+  end
+end
